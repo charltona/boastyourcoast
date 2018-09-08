5a365cc78b383f4b3e36a399c985f8f6237c8201
Aaron C <aaron_charlton@flightcentre.com>
2018-09-08 16:39:05 +1000

    updatig listing page

--- resources/views/stores/listing.blade.php
+++ resources/views/stores/listing.blade.php
@@ -1,9 +1,9 @@
 <h1>This is where we display a list of Stores</h1>
 
 @if (count($stores) > 0)
-
+<ul>
     @foreach ($stores as $store)
-        {{$store->name}}
+     <li>   {{$store->name}}</li>
     @endforeach
-
+</ul>
 @endif
