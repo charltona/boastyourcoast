ae882a53e1d630638646cd830b0f5e91ff30fcf5
Aaron C <aaron_charlton@flightcentre.com>
2018-09-08 18:32:07 +1000

    Updating collapse method for component inclusion.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,8 +10,8 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Assistan:400,700|Kalam" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link type="text/css" rel="stylesheet" href="/css/materialize.min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="/css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Styles -->
@@ -20,7 +20,7 @@
 <body>
 
     <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="/js/materialize.min.js"></script>
 
 
 
--- resources/views/stores/add.blade.php
+++ resources/views/stores/add.blade.php
@@ -1,18 +1,5 @@
-<!-- active fields
-    name - text
-    description - textarea
-    category - select
-    image - file
-    address - text
-    website - text
--->
-
-<!-- inactive fields
-    status - on import
-    owner_id - from user auth
--->
-@extends('landing');
-
+@extends('landing')
+@section ('title','Add a new listing')
 @section ('content')
 @if ($errors->any())
     <div class="alert alert-danger">
@@ -27,37 +14,36 @@
 <div class="row">
     <form action="/store" method="POST" enctype="multipart/form-data" class="col s12">
         <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s12">
                 <input type="text" id="name" name="name" class="validate">
                 <label for="name">Name</label>
             </div>
 
             <div class="input-field col s12">
                 <textarea id="description" class="materialize-textarea" name="description"></textarea>
-                <label for="description">Textarea</label>
+                <label for="description">Description</label>
             </div>
 
             <div class="input-field col s12">
-                <select name="category">
-                    <option value="" disabled selected>Choose your option</option>
+                <select name="category" class="browser-default">
+                    <option value="" disabled selected>Choose your category</option>
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
                 </select>
-                <label>Category</label>
             </div>
 
-            <div class="input-field col s6">
+            <div class="input-field col s12">
                 <input type="text" id="address" name="address" class="validate">
-                <label for="name">Address</label>
+                <label for="address">Address</label>
             </div>
 
-            <div class="input-field col s6">
+            <div class="input-field col s12">
                 <input type="text" id="website" name="website" class="validate">
-                <label for="name">Website</label>
+                <label for="website">Website</label>
             </div>
 
-            <div class="input-field col s6">
+            <div class=" col s12">
                 <label>
                     <input type="checkbox" name="featured" value="1" />
                     <span>Featured?</span>
@@ -65,27 +51,31 @@
             </div>
 
 
-            <div class="file-field input-field">
+            <div class="file-field input-field col s12">
                 <div class="btn">
-                    <span>Image</span>
+                    <span>Upload Image</span>
                     <input type="file" name="image">
                 </div>
                 <div class="file-path-wrapper">
                     <input class="file-path validate" type="text">
                 </div>
+
             </div>
 
             <!-- update these with dynamic values when ready -->
             <input type="hidden" name="owner_id" value="1">
             <input type="hidden" name="status" value="1">
 
-            <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-            </button>
+            <div class="input-field col s6">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                </button>
+            </div>
 
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
         </div>
     </form>
 </div>
+
 
 @endsection
