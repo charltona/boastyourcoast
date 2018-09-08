d313f3c1293c911d22437adb699c0c540b57fff0
Aaron C <aaron_charlton@flightcentre.com>
2018-09-08 18:09:10 +1000

    Updating store listing component and listing page Also adding image formatter thing.

--- resources/views/stores/display-box.blade.php
+++ resources/views/stores/display-box.blade.php
@@ -2,17 +2,25 @@
     USAGE:
     @include('stores.display-box', ['stores' => $stores])
  --}}
-@foreach ($stores as $store)
+<div class="row">
 
-   <div class="row">
-       <div class="col sm12-m6 l3">
+@foreach ($stores as $store)
+       <div class="col sm12 m6 l4">
            <div class="card">
+               @if ($store->image)
                <div class="card-image">
-                   <img src="/storage/{{$store->image}}" alt="{{$store->name}}">
+                   <img src="/storage/{{$store->image}}" alt="{{$store->name}}" style="max-height:200px; object-fit:cover">
+               </div>
+               @else
+                   <div class="card-image">
+                       <img src="http://lorempixel.com/640/400/food/{{$loop->index}}" alt="{{$store->name}}" class="darken-4">
+                   </div>
+                @endif
+               <div class="card-content">
                    <span class="card-title">{{$store->name}}</span>
                </div>
            </div>
        </div>
-   </div>
 
 @endforeach
+</div>
--- resources/views/stores/listing.blade.php
+++ resources/views/stores/listing.blade.php
@@ -1,4 +1,7 @@
-<h1>This is where we display a list of Stores</h1>
+@extends('landing')
+@section('title','Business Listing')
+@section ('content')
+    <h1>Business Listing</h1>
+    @include ('stores.display-box', ['stores' => $stores])
 
-
-@include ('stores.display-box', ['stores' => $stores])
+@endsection
