dee074e44f9ddbaa5988105f080e4790d7e4cf64
genevieveloreal <genevieveloreal@gmail.com>
2018-09-09 10:55:24 +1000

    Creation of visitor info page

--- resources/views/visitor-information.blade.php
+++ resources/views/visitor-information.blade.php
@@ -3,6 +3,61 @@
 
 @section('content')
 
-    {{-- content here --}}
+    <div class="visitor-info-page byc-page">
+		<div class="page-title valign-wrapper">
+			<div class="container">
+			<h1>Visitor Tips</h1>
+			</div>
+		</div>
+	
+	<div class="container inner-byc-container">
+	<div class="byc-section">
+		<div class="row">
+			<div class="col l6 s12">
+			<h4>Why visit the Sunshine Coast</h4>
+			<p>Ut hendrerit quam convallis dapibus dignissim. Sed euismod sit amet orci vel feugiat. Proin mollis placerat est, nec dictum nibh gravida at. Proin non lorem ut erat blandit egestas nec ut risus. </p></br>
+			<h4>How can I help protect the environment during my visit</h4>
+			<p>Ut hendrerit quam convallis dapibus dignissim. Sed euismod sit amet orci vel feugiat. Proin mollis placerat est, nec dictum nibh gravida at. Proin non lorem ut erat blandit egestas nec ut risus. </p>
+			</div>
+			<div class="col l6 s12">
+			<img src="/img/photo-8.jpg" style="max-width:100%;">
+			</div>
+		</div>
+	</div>
+	<div class="byc-section bg-light">
+	<div class="row">
+	<div class="col l6 s12">
+		<img src="/img/photo-7.jpg" style="max-width:100%;">
+	</div>
+	<div class="col l6 s12">
+	<h4>Things I should know</h4>
+	<p>Ut hendrerit quam convallis dapibus dignissim. Sed euismod sit amet orci vel feugiat. Proin mollis placerat est, nec dictum nibh gravida at. Proin non lorem ut erat blandit egestas nec ut risus.</p>
+	<h4>How can I support local businesses and residents</h4>
+	<p>Ut hendrerit quam convallis dapibus dignissim. Sed euismod sit amet orci vel feugiat. Proin mollis placerat est, nec dictum nibh gravida at. Proin non lorem ut erat blandit egestas nec ut risus.</p>
+	<a href="/local-business" class="btn-large byc-blue">View local businesses</a>
+	</br></br>
+	</div>
+	</div>
+	</div>
+		<div class="byc-section">
+		<div class="row">
+			<div class="col l6 s12">
+			<h4>Sustainability</h4>
+			<p>Ut hendrerit quam convallis dapibus dignissim. Sed euismod sit amet orci vel feugiat. Proin mollis placerat est, nec dictum nibh gravida at. Proin non lorem ut erat blandit egestas nec ut risus.</br></br>
+			<h4>Environmental responsibility</h4>
+			<p>Ut hendrerit quam convallis dapibus dignissim. Sed euismod sit amet orci vel feugiat. Proin mollis placerat est, nec dictum nibh gravida at. Proin non lorem ut erat blandit egestas nec ut risus.</p></div>
+			<div class="col l6 s12">
+			<img src="/img/photo-6.jpg" style="max-width:100%;">
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class="cta center" style="background-color: #ff6160; text-align: center; color: #fff; paddin-top: 40px;">
+	<div style="padding-top: 70px; padding-bottom: 70px;">
+	<p class="center" style="display: block; padding-top: 0px; margin-top: 0px; font-size: 20px;">Have concerns or feedback? We want to hear from you!</p>
+	<a class="btn-large byc-white" href="#">Contact Us</a>
+	</div>
+	</div>
+	</div>
 
 @endsection
